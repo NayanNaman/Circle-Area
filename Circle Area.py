350d62131b912c20b25773cf8c96ec45e6389696
NayanNaman <76175767+NayanNaman@users.noreply.github.com>
2021-04-02 22:38:48 +0530

    Add files via upload

---
+++ Circle Area.py
@@ -0,0 +1,5 @@
+print("Hello!, I am here to find the area of the circle.")
+r=float(input("Please enter the Radius here: "))
+area=(22/7)*r**2
+print("The area of the cicle whose radius is", r, "is", area)
+
